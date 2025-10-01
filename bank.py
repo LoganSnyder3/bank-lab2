26f824efd97ca559d750fccbcf889c3f66fbfaa6
danielkim321 <snyderl27@chcstudent.com>
2025-10-01 11:44:05 -0400

    Update bank.py

--- bank.py
+++ bank.py
@@ -3,7 +3,8 @@ print("1. View Balance")
 print("2. Deposit")
 print("3. Withdraw")
 option = input("Choose an option: ")
-students = ["john","zach","Paul","mary","susan"]
+if option == "1":
+    students = ["john","zach","Paul","mary","susan"]
 balances = [100.50,200.75,50.25,300.00,150.00]
 for i in range(len(students)):
     print(f"Student: {students[i]} Balance: {balances[i]}")
