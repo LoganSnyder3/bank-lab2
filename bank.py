7e88a254969ea511c8f2c21ac177baecb6200795
danielkim321 <snyderl27@chcstudent.com>
2025-10-02 14:45:31 -0400

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,35 +1,10 @@
-print("Welcome to the bank")
-print("1. View Balance")
-print("2. Deposit")
-print("3. Withdraw")
-option = input("Choose an option: ")
-if option == "1":
-    students = ["john","zach","Paul","mary","susan"]
-balances = [100.50,200.75,50.25,300.00,150.00]
-for i in range(len(students)):
-    print(f"Student: {students[i]} Balance: {balances[i]}")
-if option == "2":
-    input("Enter your name: ")
-    students = input()
-    if students == ("john"):
-        print("You currently have $100.50")
-    input("How much would you like to deposit? ")
-    add = input()
-    if students == ("zach"):
-        print("You currently have $200.75")
-    input("How much would you like to deposit? ")
-    add = input()
-    if students == ("paul"):
-        print("You currently have $50.25")
-        input("How much would you like to deposit? ")
-    add = input()
-    if students == ("mary"):
-        print("You currently have $300.00")
-        input("How much would you like to deposit? ")
-    add = input()
-    if students == ("susan"):
-        print("You currently have $150.00")
-        input("How much would you like to deposit? ")
-    add = input()
-        
-   
+print("- Main Menu -")
+print("1. List all accounts")
+print("2. View an account balance")
+print("3. Deposit")
+print("4. Withdraw")
+print("5. Transfer")
+print("6. Add account")
+print("7. Remove account")
+print("8. Save accounts to file")
+print("9. Quit")
