b8fdc270fdfbd2a4f792b9205cb45ccfc5f74b89
danielkim321 <snyderl27@chcstudent.com>
2025-10-02 14:28:03 -0400

    Update bank.py

--- bank.py
+++ bank.py
@@ -8,5 +8,28 @@ if option == "1":
 balances = [100.50,200.75,50.25,300.00,150.00]
 for i in range(len(students)):
     print(f"Student: {students[i]} Balance: {balances[i]}")
-elif option == "2":
-name = input("Enter your name: ")
+if option == "2":
+    input("Enter your name: ")
+    students = input()
+    if students == ("john"):
+        print("You currently have $100.50")
+    input("How much would you like to deposit? ")
+    add = input()
+    if students == ("zach"):
+        print("You currently have $200.75")
+    input("How much would you like to deposit? ")
+    add = input()
+    if students == ("paul"):
+        print("You currently have $50.25")
+        input("How much would you like to deposit? ")
+    add = input()
+    if students == ("mary"):
+        print("You currently have $300.00")
+        input("How much would you like to deposit? ")
+    add = input()
+    if students == ("susan"):
+        print("You currently have $150.00")
+        input("How much would you like to deposit? ")
+    add = input()
+        
+   
