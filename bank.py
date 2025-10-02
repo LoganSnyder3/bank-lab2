53c0cd63bf7b523f4e8646f89e86553899c038af
danielkim321 <snyderl27@chcstudent.com>
2025-10-02 14:06:32 -0400

    Update bank.py

--- bank.py
+++ bank.py
@@ -8,3 +8,5 @@ if option == "1":
 balances = [100.50,200.75,50.25,300.00,150.00]
 for i in range(len(students)):
     print(f"Student: {students[i]} Balance: {balances[i]}")
+elif option == "2":
+name = input("Enter your name: ")
