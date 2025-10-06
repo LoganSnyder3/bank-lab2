c9da709dcabb4deae911ea54a65184dcf490170e
danielkim321 <snyderl27@chcstudent.com>
2025-10-06 13:25:00 -0400

    Update bank.py

--- bank.py
+++ bank.py
@@ -8,3 +8,12 @@ print("6. Add account")
 print("7. Remove account")
 print("8. Save accounts to file")
 print("9. Quit")
+option = input("Choose an option (1-9): ")
+if option == '1':
+    print("john","zach","Paul","mary","susan")
+elif option == '2':
+    account = input("Enter account name: ")
+    if account in ["john","zach","Paul","mary","susan"]:
+        print(f"{account}'s balance is $1000")
+    else:
+        print("Account not found.")
